9d70c3066230c3710e539b942a1595c44ba46fcb
thunderbug <thunder@justforfun-gaming.com>
2017-09-14 20:51:56 +0200

    config-example.php toegevoegd en config.php aan gitignore

---
+++ inc/config-example.php
@@ -0,0 +1,19 @@
+<?php
+//MySQL Database
+
+//Database host
+$dbHost = "localhost";
+//Database naam
+$dbName = "boekenzoekers";
+//Database username
+$dbUsername = "boekenzoekers";
+//Database password
+$dbPassword = "databasepassword";
+
+//FB Developer
+//https://developers.facebook.com/
+
+//App ID
+$appID = "";
+//App Secret
+$appSecret = "";
